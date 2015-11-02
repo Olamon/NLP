a28054660d68376e71e292302892f52496545e53
Olenka1810 <aleksandra.spyra180@gmail.com>
2015-11-02 22:33:26 +0100

    final_generate

--- random_text_generator.py
+++ random_text_generator.py
@@ -7,33 +7,54 @@ import sys
     
 class RandomTextGenerator:
   
-  def __init__(self):
-    self.mapping = dbm.open('ngrams_mapping_uniform', 'r')
+  def __init__(self, bigram_k, trigram_k):
+    self.mapping = dict([])
+    #key_set_const = '_key_set_'
+    for line in open('2grams', 'r'):
+      cnt,w1,w2 = line.strip().split(' ')
+      if int(cnt) < int(bigram_k):
+          continue
+      value = self.mapping.get('#' + w1, '') + '#' + w2
+      #key_set = self.mapping.get(key_set_const, '') + '#' + w1
+      self.mapping['#' + w1] = value
+      #self.mapping[key_set_const] = key_set
+    for line in open('3grams', 'r'):
+      cnt,w1,w2,w3 = line.strip().split(' ')
+      if int(cnt) < int(trigram_k):
+          continue
+      key = w1 + ' ' + w2
+      value = self.mapping.get(key, '') + '#' + w3
+      #key_set = self.mapping.get(key_set_const, '') + '#' + key
+      self.mapping[key] = value
+      #self.mapping[key_set_const] = key_set
 	
   def possible_next_for(self, phrase):
-      next_words = (self.mapping.get(phrase, '#')).split('#')[1:]
-      if not next_words:
-          return None
-      index = random.randint(0, len(next_words) -1)
-      return next_words[index]
+    next_words = (self.mapping.get(phrase, '#')).split('#')[1:]
+    if not next_words:
+        return None
+    index = random.randint(0, len(next_words) -1)
+    return next_words[index]
+  
+  def get_seed(self):
+    possible_keys = [x for x in self.mapping if x[0] != '#']
+    index = random.randint(0, len(possible_keys) -1)
+    return possible_keys[index]
   
   def generate_text(self):
-    word = self.possible_next_for('_key_set_')
-    result = [word]
-    print word
-    next_phrase = self.possible_next_for(result[-1])
-    while next_phrase:
-      print ' ' + next_phrase
-      result = [result[-1], next_phrase]
-      next_phrase = self.possible_next_for(result[0] + ' ' + result[1])
-      if not next_phrase:
-          next_phrase = self.possible_next_for(result[1])
-
-  def __del__(self):
-    self.mapping.close()
-
+    phrase = self.get_seed()
+    print phrase
+    w1,w2 = phrase.split(' ')
+    next_word = self.possible_next_for(phrase)
+    while next_word:
+      phrase = w2 + next_word
+      print ' ' + next_word
+      w2 = next_word
+      next_word = self.possible_next_for(phrase)
+      if not next_word:
+          next_word = self.possible_next_for('#' + w2)
+          
 def main():
-    random_text_generator = RandomTextGenerator()
+    random_text_generator = RandomTextGenerator(100, 50)
     input_text = raw_input('More? If so type \'Y\'.')
     while input_text == 'Y':
         random_text_generator.generate_text()
--- random_text_generator_with_weights.py
+++ random_text_generator_with_weights.py
@@ -1,18 +1,38 @@
 #-*- coding: utf-8 -*-
 import codecs
+import collections
 import dbm
 import numpy
 import sys
 
 class RandomTextGeneratorWithWeights:
-    def __init__(self):
-        self.mapping = dbm.open('ngrams_mapping_weighted', 'r')
-
+    def __init__(self, bigram_k, trigram_k):
+        self.mapping = dict([])
+        self.bigram_cnt = 0.0
+        for line in open('2grams', 'r'):
+            cnt,w1,w2 = line.strip().split(' ')
+            if int(cnt) < int(bigram_k):
+                continue
+            value = self.mapping.get('#' + w1, '') + ('#' + cnt + '^' + w2)
+            self.mapping['#' + w1] = value
+            self.bigram_cnt += float(cnt)
+        for line in open('3grams', 'r'):
+            cnt,w1,w2,w3 = line.strip().split(' ')
+            if int(cnt) < int(trigram_k)
+                continue
+            value = self.mapping.get(w1 + ' ' + w2, '') + ('#' + cnt + '^' + w3)
+            self.mapping[w1 + ' ' + w2] = value
+ 
     def get_bigram_seed(self):
-        all_count = float(self.mapping.get('_bigram_cnt_', ''))
-        list_with_keys = self.mapping.get('_key_set_', '').split('#')
-        words_and_counts = [list(t) for t in zip(*[(x.split('^')[1], int(x.split('^')[0])) for x in list_with_keys if '^' in x])]
-        return numpy.random.choice(words_and_counts[0], p=[x/all_count for x in words_and_counts[1]])
+        words = []
+        counts = []
+        for w1 in self.mapping:
+            if w1[0] == '#':
+            for cnt_and_w2 in self.mapping[w1].split('#'):
+                if '^' in cnt_and_w2:
+                    words.append(cnt_and_w2.split('^')[1])
+                    counts.append(float(cnt_and_w2.split('^')[0]/self.bigram_cnt))
+        return numpy.random.choice(words, p=counts)
 
     def generate_text(self):
         phrase = self.get_bigram_seed()
@@ -30,7 +50,7 @@ class RandomTextGeneratorWithWeights:
                 next_word_possible = self.mapping.get('#' + phrase.split(' ')[1]).split('#')[1:]
 
 def main():
-    random_text_generator = RandomTextGeneratorWithWeights()
+    random_text_generator = RandomTextGeneratorWithWeights(25, 25)
     input_text = raw_input('More? If so type \'Y\'.')
     while input_text == 'Y':
             random_text_generator.generate_text()
